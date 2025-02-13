310c90abe3be861fba5c38683d505086e1282585
Matheus Melo <rmelomatheus@gmail.com>
2025-02-12 22:55:04 -0300

    new file:   README.md 	new file:   eslint.config.js 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   src/App.jsx 	new file:   src/assets/logo/spotify-logo.png 	new file:   src/assets/logo/spotify-logo1.png 	new file:   src/components/Header.jsx 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
+
+// rafce
+// Arrow Function
+// const App2 = () => <h1>Olá, Mundo!</h1>;
+// Nomeação de Componente
+// PascalCase
+// export default, posso importar com qualquer nome e sem chaves
+// export "sem default", só posso importar entre chaves e com o nome exportado
+// Self closing tag
+// <Header></Header>
+// <Header/>
